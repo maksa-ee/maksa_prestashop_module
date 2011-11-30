4e6152a9874597b09eba31d94d3bd0757217c5d7
Alex Rudakov <alexandr.rudakov@modera.net>
2011-11-30 13:44:53 +0200

    Russian and estonian translations added

---
+++ et.php
@@ -0,0 +1,18 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{maksa}prestashop>maksa_c888438d14855d7d96a2724ee9c306bd'] = 'Seaded uuendatud';
+$_MODULE['<{maksa}prestashop>maksa_51bd13ec273b011e71a73b9c6f617253'] = 'Maksa seadused';
+$_MODULE['<{maksa}prestashop>maksa_b17f3f4dcf653a5776792498a9b44d6a'] = 'Uuenda seaded';
+$_MODULE['<{maksa}prestashop>maksa_258dc9e65340e492e5e8833f6d213023'] = 'Maksmine läbi Maksa.ee';
+$_MODULE['<{maksa}prestashop>payment_a240fa27925a635b08dc28c9e4f9216d'] = 'Tellinmus';
+$_MODULE['<{maksa}prestashop>payment_2b4f35547956e80ebbfcfe1082d8488f'] = 'Maksa.ee makseviis';
+$_MODULE['<{maksa}prestashop>payment_569fd05bdafa1712c4f6be5b153b8418'] = 'Muud makseviisid';
+$_MODULE['<{maksa}prestashop>payment_258dc9e65340e492e5e8833f6d213023'] = 'Maksmine läbi Maksa.ee';
+$_MODULE['<{maksa}prestashop>response_6e502f6a51b2a5581a276c3968595134'] = 'Maksmine OK';
+$_MODULE['<{maksa}prestashop>response_872a5e1e8663c52aa0a79bb42bf99b3d'] = 'Maksmise rike';
+$_MODULE['<{maksa}prestashop>waiting_1fe3bb016fa11dd93fb1929f118fd492'] = 'Palun oodake kuni teie tellimuse töödeldakse ...';
+$_MODULE['<{maksa}prestashop>waiting_0e17bce38441dcc87a284040d513c9f7'] = 'Maksmine edukalt sooritatud!';
+$_MODULE['<{maksa}prestashop>waiting_374498fc35cc1ad3ad6e7cff5fec630b'] = 'Maksmise viga!';
+$_MODULE['<{maksa}prestashop>waiting_78ffdfb25bba4f27332df59b61ebaa22'] = 'Ava tellimus';
---
+++ ru.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{maksa}prestashop>maksa_444bcb3a3fcf8389296c49467f27e1d6'] = 'ok';
+$_MODULE['<{maksa}prestashop>maksa_c888438d14855d7d96a2724ee9c306bd'] = 'Настройки обновлены';
+$_MODULE['<{maksa}prestashop>maksa_51bd13ec273b011e71a73b9c6f617253'] = 'Настройки модуля maksa';
+$_MODULE['<{maksa}prestashop>maksa_b17f3f4dcf653a5776792498a9b44d6a'] = 'Обновить настройки';
+$_MODULE['<{maksa}prestashop>maksa_258dc9e65340e492e5e8833f6d213023'] = 'Оплата через maksa.ee';
+$_MODULE['<{maksa}prestashop>payment_a240fa27925a635b08dc28c9e4f9216d'] = 'Заказ';
+$_MODULE['<{maksa}prestashop>payment_2b4f35547956e80ebbfcfe1082d8488f'] = 'Оплата через Maksa.ee';
+$_MODULE['<{maksa}prestashop>payment_569fd05bdafa1712c4f6be5b153b8418'] = 'Другие способы оплаты';
+$_MODULE['<{maksa}prestashop>payment_258dc9e65340e492e5e8833f6d213023'] = 'Оплатить через Maksa.ee';
+$_MODULE['<{maksa}prestashop>response_6e502f6a51b2a5581a276c3968595134'] = 'Оплата ОК';
+$_MODULE['<{maksa}prestashop>response_872a5e1e8663c52aa0a79bb42bf99b3d'] = 'Ошибка оплаты';
+$_MODULE['<{maksa}prestashop>waiting_1fe3bb016fa11dd93fb1929f118fd492'] = 'Подождите, пока ваш заказ обрабатывается ...';
+$_MODULE['<{maksa}prestashop>waiting_0e17bce38441dcc87a284040d513c9f7'] = 'Оплата прошла удачно!';
+$_MODULE['<{maksa}prestashop>waiting_374498fc35cc1ad3ad6e7cff5fec630b'] = 'Ошибка при оплате!';
+$_MODULE['<{maksa}prestashop>waiting_78ffdfb25bba4f27332df59b61ebaa22'] = 'Перейти к заказу';
