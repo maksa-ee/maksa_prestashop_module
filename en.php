e28a9aec5cd4bfade8583e5ef566407bd999ea93
Alex Rudakov <alexandr.rudakov@modera.net>
2011-12-14 18:42:30 +0200

    Fixing texts

--- en.php
+++ en.php
@@ -2,4 +2,4 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{maksa}prestashop>maksa_8bdc84c526abfb573cf6dbf08f3004e0'] = 'Maksa.ee. Safe payment with bank transfer';
+$_MODULE['<{maksa}prestashop>maksa_8bdc84c526abfb573cf6dbf08f3004e0'] = 'Maksa.ee. Payment with internet bank';
